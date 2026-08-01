7a872819ab186a4bba956692619740eaf33accd6
with-page <ccc26676@gmail.com>
2026-08-02 03:55:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const lockScreen =
+document.getElementById("lockScreen");
 
+const homeScreen =
+document.getElementById("homeScreen");
+
+
+
+lockScreen.addEventListener(
+"click",
+()=>{
+
+
+lockScreen.style.display="none";
+
+homeScreen.style.display="block";
+
+
+});
+
+
+
+
+
+function openApp(app){
+
+alert(
+app + " 앱을 열었습니다."
+);
+
+
+}
